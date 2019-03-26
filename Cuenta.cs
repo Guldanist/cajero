9dabf86d804a08eb4ee9248760320dea613c404c
Guldanist <45608585+Guldanist@users.noreply.github.com>
2019-03-26 08:05:36 -0500

    Add files via upload

---
+++ Cuenta.cs
@@ -0,0 +1,45 @@
+﻿namespace cajeroAutomatico
+{
+    class Cuenta
+    {
+        string nroCuenta;
+        double monto;
+        char moneda;
+        public string NroCuenta
+        {
+            get
+            {
+                return nroCuenta;
+            }
+
+            set
+            {
+                nroCuenta = value;
+            }
+        }
+        public double Monto
+        {
+            get
+            {
+                return monto;
+            }
+
+            set
+            {
+                monto = value;
+            }
+        }
+        public char Moneda
+        {
+            get
+            {
+                return moneda;
+            }
+
+            set
+            {
+                moneda = value;
+            }
+        }
+    }
+}
